fd71ff115461e6c4564180c84d22ebb16ca5ac71
Luis del Cueto <luisdelcueto@Luiss-MBP-2.home>
2017-07-02 00:04:47 -0400

    Done.

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,1 +1,29 @@
 // Component Code Goes Here
+import React from 'react'
+
+class ToggleButton extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      isEnabled: false
+    }
+  this.change = this.change.bind(this)
+  }
+
+  change() {
+    this.setState({
+      isEnabled: !this.state.isEnabled
+    })
+  }
+
+  render() {
+    return (
+      <button className="toggle-button" onClick={this.change}>
+        I am toggled {this.state.isEnabled ? 'on' : 'off'}
+      </button>
+    )
+  }
+}
+
+export default ToggleButton
